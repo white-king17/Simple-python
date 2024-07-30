b186815efc845b2da9cea997e5e3f1c7a5a08cb8
white-king17 <learningdhanush@gmail.com>
2024-07-31 00:40:26 +0530

    moved

--- README.md
+++
@@ -1,2 +0,0 @@
-# This is my GitHub Account
- ***This my GitHub account which contains my c programs and python programs***
--- lenthlastword.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        words = s.split()
-        last=words[-1]
-        return len(last)
